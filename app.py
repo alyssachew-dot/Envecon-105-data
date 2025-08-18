114240bd27a1fbd37b21bda338f9fd0bd828a7e3
alyssachew-dot <alyssachew@berkeley.edu>
2025-08-17 18:42:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+import statsmodels.formula.api as smf
+import seaborn as sns
+from adjustText import adjust_text
+
 # -----------------------
 # Title and description
 # -----------------------
-st.title("Interactive Dashboard Example")
-st.write("""
-This is a simple dashboard demonstrating interactive widgets, charts, and layout using Streamlit.
-""")
+st.title("Alyssa Chew Envecon 105 Dashboard")
+st.write("""This is a simple dashboard demonstrating interactive widgets, charts, and layout using Streamlit.""")
+
 
   
 
